d209e54d1775487a9ccd80b6c3003962542fc553
Lahaela <dicksteinleah@gmail.com>
2014-04-05 22:38:11 -0700

    Buyer Sketches + Init Data Vars
    Main data points are stored as variables, time to play around with
    Firebase

--- Framework7-master/dist/js/bobarocket.js
+++
@@ -1,8 +0,0 @@
-        $$('.boba-prompt').tap(function () {
-            myApp.prompt('What is your name?', function (data) {
-                // @data contains input value
-                myApp.confirm('Are you sure that your name is ' + data + '?', function () {
-                    myApp.alert('Ok, your name is ' + data + ' ;)');
-                });
-            });
-        });
--- Framework7-master/dist/js/my-app.js
+++ Framework7-master/dist/js/my-app.js
@@ -30,6 +30,44 @@ $$(document).on('pageInit', function (e) {
                 myApp.prompt('And number?', function(data) {phonenum=data;});
             });
         });
+    // Pull To Refresh Demo
+    if (page.name === 'arrivalorders') {
+        // Dummy Content
+        var songs = ['Yellow Submarine', 'Don\'t Stop Me Now', 'Billie Jean', 'Californication'];
+        var authors = ['Beatles', 'Queen', 'Michael Jackson', 'Red Hot Chili Peppers'];
+        // Pull to refresh content
+        var ptrContent = $$(page.container).find('.pull-to-refresh-content');
+        // Add 'refresh' listener on it
+        ptrContent.on('refresh', function (e) {
+            // Emulate 2s loading
+            setTimeout(function () {
+                var picURL = 'http://hhhhold.com/88/d/jpg?' + Math.round(Math.random() * 100);
+                var song = songs[Math.floor(Math.random() * songs.length)];
+                var author = authors[Math.floor(Math.random() * authors.length)];
+                var linkHTML = '<li class="item-content">' +
+                                    '<div class="item-media"><img src="' + picURL + '" width="44"/></div>' +
+                                    '<div class="item-inner">' +
+                                        '<div class="item-title-row">' +
+                                            '<div class="item-title">' + song + '</div>' +
+                                        '</div>' +
+                                        '<div class="item-subtitle">' + author + '</div>' +
+                                    '</div>' +
+                                '</li>';
+                ptrContent.find('ul').prepend(linkHTML);
+                // When loading done, we need to "close" it
+                myApp.pullToRefreshDone();
+            }, 2000);
+        });
+    }
+
+    //Location Data Puller
+        if (page.name === 'locations') {
+        $$(".item-content").tap(function(event) { //replace item-title with whatever var location is
+        //console.log(.item-title);
+        var test = $$(".item-title")
+        var loc = test.attr("id")
+});
+};
 
 });
 
@@ -66,4 +104,3 @@ function createContentPage(){
     );
 	return;
 }
-
